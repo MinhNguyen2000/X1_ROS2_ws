c047c66dde46ffc9b6b4e69356b701175beeb14e
Minh Nguyen <mnguyen6@unb.ca>
2025-07-25 14:52:42 -0300

    minor changes to ros_entrypoint and vscode settings

--- .devcontainer/ros_entrypoint.sh
+++ .devcontainer/ros_entrypoint.sh
@@ -5,9 +5,10 @@
 export ROS_DOMAIN_ID=6
 export ROS_LOCALHOST_ONLY=0
 export ROS_IP=$(hostname -I | awk '{print $1}')
+export LIDAR_TYPE=x3
 
 echo "----------"
-echo -e "ROS_DOMAIN_ID:$ROS_DOMAIN_ID"
-echo -e "ROS_IP:$ROS_IP"
+echo -e "ROS_DOMAIN_ID:\033[32m$ROS_DOMAIN_ID\033[0m | ROS_IP:\033[32m$ROS_IP\033[0m"
+echo -e "LIDAR_TYPE: \033[32m$LIDAR_TYPE\033[0m"
 echo "----------"
 exec "$@"
