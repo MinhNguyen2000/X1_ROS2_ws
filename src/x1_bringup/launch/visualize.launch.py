34878f96edb2b812f0e323872dd177b77ac30f73
Minh Nguyen <mnguyen6@unb.ca>
2025-07-25 16:50:54 -0300

    create launch file to visualize real robot data on workstation
    - start the robot_state_publisher, joint_state_publisher, and rviz nodes
    - modify the rviz config file with added LiDAR plugin in RViz

---
+++ src/x1_bringup/launch/visualize.launch.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node 
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    
+    model_arg = DeclareLaunchArgument(
+        name = "model",
+        default_value = os.path.join(
+            get_package_share_directory("x1_description"),"urdf","x1.urdf.xacro"),
+        description="Absolute path to robot URDF file"
+    )
+
+    model = LaunchConfiguration("model")
+    robot_description = ParameterValue(
+        Command(["xacro ", model]),
+        value_type = str
+    )
+
+    # 
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters = [{"robot_description": robot_description}],
+    )
+    
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher'
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", os.path.join(
+            get_package_share_directory("x1_description"),
+            "rviz","real.rviz")]
+    )
+
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz
+    ])
