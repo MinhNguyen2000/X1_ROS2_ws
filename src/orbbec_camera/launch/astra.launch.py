ea89b65a7048aeac2767a0004be63ec59fa7648f
Minh Nguyen <mnguyen6@unb.ca>
2025-07-29 15:08:23 -0300

    create yaml file for camera parameters instead of declaring all arguments in a launch file

--- src/orbbec_camera/launch/astra.launch.py
+++ src/orbbec_camera/launch/astra.launch.py
@@ -8,80 +8,94 @@ from launch_ros.actions import Node
 from launch_ros.actions import PushRosNamespace
 from launch_ros.descriptions import ComposableNode
 
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     # Declare arguments
-    args = [
-        DeclareLaunchArgument('camera_name', default_value='camera'),
-        DeclareLaunchArgument('depth_registration', default_value='false'),
-        DeclareLaunchArgument('serial_number', default_value=''),
-        DeclareLaunchArgument('usb_port', default_value=''),
-        DeclareLaunchArgument('device_num', default_value='1'),
-        DeclareLaunchArgument('vendor_id', default_value='0x2bc5'),
-        DeclareLaunchArgument('product_id', default_value=''),
-        DeclareLaunchArgument('enable_point_cloud', default_value='true'),
-        DeclareLaunchArgument('enable_colored_point_cloud', default_value='false'),
-        DeclareLaunchArgument('cloud_frame_id', default_value=''),
-        DeclareLaunchArgument('point_cloud_qos', default_value='default'),
-        DeclareLaunchArgument('connection_delay', default_value='100'),
-        DeclareLaunchArgument('color_width', default_value='640'),
-        DeclareLaunchArgument('color_height', default_value='480'),
-        DeclareLaunchArgument('color_fps', default_value='10'),
-        DeclareLaunchArgument('color_format', default_value='RGB'),
-        DeclareLaunchArgument('enable_color', default_value='true'),
-        DeclareLaunchArgument('flip_color', default_value='false'),
-        DeclareLaunchArgument('color_qos', default_value='default'),
-        DeclareLaunchArgument('color_camera_info_qos', default_value='default'),
-        DeclareLaunchArgument('enable_color_auto_exposure', default_value='true'),
-        DeclareLaunchArgument('color_exposure', default_value='-1'),
-        DeclareLaunchArgument('color_gain', default_value='-1'),
-        DeclareLaunchArgument('enable_color_auto_white_balance', default_value='true'),
-        DeclareLaunchArgument('color_white_balance', default_value='-1'),
-        DeclareLaunchArgument('depth_width', default_value='640'),
-        DeclareLaunchArgument('depth_height', default_value='480'),
-        DeclareLaunchArgument('depth_fps', default_value='10'),
-        DeclareLaunchArgument('depth_format', default_value='Y11'),
-        DeclareLaunchArgument('enable_depth', default_value='true'),
-        DeclareLaunchArgument('flip_depth', default_value='false'),
-        DeclareLaunchArgument('depth_qos', default_value='default'),
-        DeclareLaunchArgument('depth_camera_info_qos', default_value='default'),
-        DeclareLaunchArgument('ir_width', default_value='640'),
-        DeclareLaunchArgument('ir_height', default_value='480'),
-        DeclareLaunchArgument('ir_fps', default_value='10'),
-        DeclareLaunchArgument('ir_format', default_value='Y10'),
-        DeclareLaunchArgument('enable_ir', default_value='false'),
-        DeclareLaunchArgument('flip_ir', default_value='false'),
-        DeclareLaunchArgument('ir_qos', default_value='default'),
-        DeclareLaunchArgument('ir_camera_info_qos', default_value='default'),
-        DeclareLaunchArgument('enable_ir_auto_exposure', default_value='true'),
-        DeclareLaunchArgument('ir_exposure', default_value='-1'),
-        DeclareLaunchArgument('ir_gain', default_value='-1'),
-        DeclareLaunchArgument('publish_tf', default_value='true'),
-        DeclareLaunchArgument('tf_publish_rate', default_value='0.0'),
-        DeclareLaunchArgument('ir_info_url', default_value=''),
-        DeclareLaunchArgument('color_info_url', default_value=''),
-        DeclareLaunchArgument('log_level', default_value='none'),
-        DeclareLaunchArgument('enable_publish_extrinsic', default_value='false'),
-        DeclareLaunchArgument('enable_d2c_viewer', default_value='false'),
-        DeclareLaunchArgument('enable_ldp', default_value='true'),
-        DeclareLaunchArgument('enable_soft_filter', default_value='true'),
-        DeclareLaunchArgument('soft_filter_max_diff', default_value='-1'),
-        DeclareLaunchArgument('soft_filter_speckle_size', default_value='-1'),
-        DeclareLaunchArgument('ordered_pc', default_value='false'),
-        DeclareLaunchArgument('use_hardware_time', default_value='false'),
-        DeclareLaunchArgument('enable_depth_scale', default_value='true'),
-        DeclareLaunchArgument('align_mode', default_value='HW'),
-        DeclareLaunchArgument('laser_energy_level', default_value='-1'),
-        DeclareLaunchArgument('enable_heartbeat', default_value='false'),
-    ]
+    # args = [
+    #     DeclareLaunchArgument('camera_name', default_value='camera'),
+    #     DeclareLaunchArgument('depth_registration', default_value='false'),
+    #     DeclareLaunchArgument('serial_number', default_value=''),
+    #     DeclareLaunchArgument('usb_port', default_value=''),
+    #     DeclareLaunchArgument('device_num', default_value='1'),
+    #     DeclareLaunchArgument('vendor_id', default_value='0x2bc5'),
+    #     DeclareLaunchArgument('product_id', default_value=''),
+    #     DeclareLaunchArgument('enable_point_cloud', default_value='true'),
+    #     DeclareLaunchArgument('enable_colored_point_cloud', default_value='false'),
+    #     DeclareLaunchArgument('cloud_frame_id', default_value=''),
+    #     DeclareLaunchArgument('point_cloud_qos', default_value='default'),
+    #     DeclareLaunchArgument('connection_delay', default_value='100'),
+    #     DeclareLaunchArgument('color_width', default_value='640'),
+    #     DeclareLaunchArgument('color_height', default_value='480'),
+    #     DeclareLaunchArgument('color_fps', default_value='10'),
+    #     DeclareLaunchArgument('color_format', default_value='RGB'),
+    #     DeclareLaunchArgument('enable_color', default_value='true'),
+    #     DeclareLaunchArgument('flip_color', default_value='false'),
+    #     DeclareLaunchArgument('color_qos', default_value='default'),
+    #     DeclareLaunchArgument('color_camera_info_qos', default_value='default'),
+    #     DeclareLaunchArgument('enable_color_auto_exposure', default_value='true'),
+    #     DeclareLaunchArgument('color_exposure', default_value='-1'),
+    #     DeclareLaunchArgument('color_gain', default_value='-1'),
+    #     DeclareLaunchArgument('enable_color_auto_white_balance', default_value='true'),
+    #     DeclareLaunchArgument('color_white_balance', default_value='-1'),
+    #     DeclareLaunchArgument('depth_width', default_value='640'),
+    #     DeclareLaunchArgument('depth_height', default_value='480'),
+    #     DeclareLaunchArgument('depth_fps', default_value='10'),
+    #     DeclareLaunchArgument('depth_format', default_value='Y11'),
+    #     DeclareLaunchArgument('enable_depth', default_value='true'),
+    #     DeclareLaunchArgument('flip_depth', default_value='false'),
+    #     DeclareLaunchArgument('depth_qos', default_value='default'),
+    #     DeclareLaunchArgument('depth_camera_info_qos', default_value='default'),
+    #     DeclareLaunchArgument('ir_width', default_value='640'),
+    #     DeclareLaunchArgument('ir_height', default_value='480'),
+    #     DeclareLaunchArgument('ir_fps', default_value='10'),
+    #     DeclareLaunchArgument('ir_format', default_value='Y10'),
+    #     DeclareLaunchArgument('enable_ir', default_value='false'),
+    #     DeclareLaunchArgument('flip_ir', default_value='false'),
+    #     DeclareLaunchArgument('ir_qos', default_value='default'),
+    #     DeclareLaunchArgument('ir_camera_info_qos', default_value='default'),
+    #     DeclareLaunchArgument('enable_ir_auto_exposure', default_value='true'),
+    #     DeclareLaunchArgument('ir_exposure', default_value='-1'),
+    #     DeclareLaunchArgument('ir_gain', default_value='-1'),
+    #     DeclareLaunchArgument('publish_tf', default_value='true'),
+    #     DeclareLaunchArgument('tf_publish_rate', default_value='0.0'),
+    #     DeclareLaunchArgument('ir_info_url', default_value=''),
+    #     DeclareLaunchArgument('color_info_url', default_value=''),
+    #     DeclareLaunchArgument('log_level', default_value='none'),
+    #     DeclareLaunchArgument('enable_publish_extrinsic', default_value='false'),
+    #     DeclareLaunchArgument('enable_d2c_viewer', default_value='false'),
+    #     DeclareLaunchArgument('enable_ldp', default_value='true'),
+    #     DeclareLaunchArgument('enable_soft_filter', default_value='true'),
+    #     DeclareLaunchArgument('soft_filter_max_diff', default_value='-1'),
+    #     DeclareLaunchArgument('soft_filter_speckle_size', default_value='-1'),
+    #     DeclareLaunchArgument('ordered_pc', default_value='false'),
+    #     DeclareLaunchArgument('use_hardware_time', default_value='false'),
+    #     DeclareLaunchArgument('enable_depth_scale', default_value='true'),
+    #     DeclareLaunchArgument('align_mode', default_value='HW'),
+    #     DeclareLaunchArgument('laser_energy_level', default_value='-1'),
+    #     DeclareLaunchArgument('enable_heartbeat', default_value='false'),
+    # ]
 
     # Node configuration
-    parameters = [{arg.name: LaunchConfiguration(arg.name)} for arg in args]
+    params_file_arg = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(
+            get_package_share_directory('orbbec_camera'),
+            'config',
+            'camera_params.yaml'
+        ),
+        description='Full path to the YAML parameters file to load',
+    ),
+
+    parameters = LaunchConfiguration('params_file')
+
     # get  ROS_DISTRO
     ros_distro = os.environ["ROS_DISTRO"]
     if ros_distro == "foxy":
         return LaunchDescription(
-            args
+            # args
+            params_file_arg
             + [
                 Node(
                     package="orbbec_camera",
@@ -115,12 +129,10 @@ def generate_launch_description():
             output="screen",
         )
         # Launch description
-        ld = LaunchDescription(
-            args
-            + [
-                GroupAction(
-                    [PushRosNamespace(LaunchConfiguration("camera_name")), container]
-                )
-            ]
-        )
+        ld = LaunchDescription([
+            params_file_arg,
+            GroupAction(
+                [PushRosNamespace(LaunchConfiguration("camera_name")), container]
+            )
+        ])
         return ld
