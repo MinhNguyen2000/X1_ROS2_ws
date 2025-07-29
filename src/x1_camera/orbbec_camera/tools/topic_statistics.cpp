506d21c5b6210d8c1675265e62dfd2a61c8e94c1
Minh Nguyen <mnguyen6@unb.ca>
2025-07-29 10:43:11 -0300

    remove x1_camera pacakge (build fail)

--- src/x1_camera/orbbec_camera/tools/topic_statistics.cpp
+++
@@ -1,8 +0,0 @@
-#include "topic_statistics.hpp"
-
-int main(int argc, char * argv[]) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<orbbec_camera::tools::TopicStatistics>());
-  rclcpp::shutdown();
-  return 0;
-}
