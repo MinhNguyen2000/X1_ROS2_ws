72ee9ec90b2307c9f98119373f0a0c782cd9c1c3
Minh Nguyen <mnguyen6@unb.ca>
2026-04-16 20:18:08 +0000

    add rosmaster_driver package for ROS2 and sensor/actuator communication

---
+++ src/rosmaster_driver_install/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Rosmaster_Lib',
+    version='3.3.9',
+    author='Yahboom Team',
+    packages=find_packages(),
+)
+
+# cd rosmaster_driver_install
+# sudo python3 setup.py install
