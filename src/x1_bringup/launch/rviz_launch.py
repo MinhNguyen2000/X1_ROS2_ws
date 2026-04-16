e4bd60f6feab30026b3f16365abe8340bd644404
Minh Nguyen <mnguyen6@unb.ca>
2026-04-16 20:24:42 +0000

    modify conditional joint state publisher launching
    not launched when working with physical hardware, launched when woking with Gazebo

--- src/x1_bringup/launch/rviz_launch.py
+++ src/x1_bringup/launch/rviz_launch.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration
 
@@ -12,6 +13,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    # ===== DECLARE LAUNCH ARGUMENTS =====
     model_arg = DeclareLaunchArgument(
         name = "model",
         default_value = os.path.join(
@@ -19,13 +21,20 @@ def generate_launch_description():
         description="Absolute path to robot URDF file"
     )
 
+    use_gazebo_arg = DeclareLaunchArgument(
+        name="use_gazebo",
+        default_value="true",
+        description="Set to false when running on real hardware (robot publishes joint states itself)"
+    )
+
     model = LaunchConfiguration("model")
+    use_gazebo = LaunchConfiguration("use_gazebo")
+
     robot_description = ParameterValue(
         Command(["xacro ", model]),
         value_type = str
     )
 
-    # 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -34,7 +43,8 @@ def generate_launch_description():
     
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
-        executable='joint_state_publisher'
+        executable='joint_state_publisher',
+        condition=IfCondition(use_gazebo)
     )
 
     image_transport_node = Node(
@@ -56,6 +66,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         model_arg,
+        use_gazebo_arg,
         robot_state_publisher_node,
         joint_state_publisher_node,
         image_transport_node,
