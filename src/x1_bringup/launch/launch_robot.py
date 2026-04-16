f58efe7ac5c9a32efc4ec09bd1db7276fc192926
Minh Nguyen <mnguyen6@unb.ca>
2026-04-16 16:42:26 +0000

    modify launch_robot file to launch the driver node

--- src/x1_bringup/launch/launch_robot.py
+++ src/x1_bringup/launch/launch_robot.py
@@ -16,12 +16,23 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Path to the ydlidar_ros2_driver launch file
+    '''
+    Function to launch the following functionalities on the robot
+      1. LiDAR - using the ydlidar package launch file
+      2. Camera - using the orbbec camera package
+      3. The IMU
+      4. Odometry components - LiDAR scan matching, EKF sensor fusion
+    '''
+    # --- Define required paths
+    camera_pkg_dir = get_package_share_directory("orbbec_camera")
+    camera_params_path = os.path.join(camera_pkg_dir, "config", "camera_params.yaml")
+
+    # --- Define the launch arguments
     ydlidar_launch_file_dir = os.path.join(
         get_package_share_directory('ydlidar_ros2_driver'),
         'launch',
         'ydlidar_launch.py'
-    )
+    )   # path to the ydlidar_ros2_driver launch file
 
     camera_name_arg = DeclareLaunchArgument(
         'camera_name',
@@ -31,21 +42,20 @@ def generate_launch_description():
 
     camera_params_file_arg = DeclareLaunchArgument(
         'camera_params_file',
-        default_value=os.path.join(
-            get_package_share_directory('orbbec_camera'),
-            'config',
-            'camera_params.yaml'
-        ),
+        default_value=camera_params_path,
         description='Full path to the YAML parameters file to load',
     )
 
     camera_name = LaunchConfiguration("camera_name")
     camera_params = LaunchConfiguration('camera_params_file')
 
+    # ===== NODES & LAUNCH DESCRIPTIONS =====
+    # lidar node
     ydlidar = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(ydlidar_launch_file_dir)
     )
 
+    # camera nodes
     compose_node = ComposableNode(
         package="orbbec_camera",
         plugin="orbbec_camera::OBCameraNodeDriver",
@@ -53,6 +63,7 @@ def generate_launch_description():
         namespace="",
         parameters=[camera_params],
     )
+
     # Define the ComposableNodeContainer
     camera_container = ComposableNodeContainer(
         name="camera_container",
@@ -65,6 +76,12 @@ def generate_launch_description():
         output="screen",
     )
 
+    # Low level driver node
+    driver_node = Node(
+        package='x1_bringup',
+        executable='Mcnamu_driver',
+    )
+
     return LaunchDescription([
         camera_name_arg,
         camera_params_file_arg,
@@ -72,5 +89,6 @@ def generate_launch_description():
         GroupAction([
             PushRosNamespace(LaunchConfiguration("camera_name")), 
             camera_container
-        ])
+        ]),
+        driver_node
     ])
