6fa1f979e271be54031faa17353a48195b7bd3b4
Minh Nguyen <mnguyen6@unb.ca>
2026-04-21 16:32:18 +0000

    initial odometry EKF tuning effort
    in ekf_params.yaml
    - changed the wheel velocity from odom0 to twist0 (TwistWithCovarianceStamped message, not Odometry)
    - using wheel vyaw
    - no longer fusing vx, vy, vyaw from lidar
    
    in odom_launch.py
    - reduced laser scan matching frequency for less error/covariance accumulation
    
    in covariance_filter.py
    - tuned the covariance for all odometry sources
    - implemented a low pass filter for LiDAR scan's yaw measurement to reduce yaw drift

--- src/x1_bringup/launch/odom_launch.py
+++ src/x1_bringup/launch/odom_launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
             "base_frame_id" : "base_footprint",
             "odom_frame_id" : "odom",
             "init_pose_from_topic" : "",
-            "freq" : 20.0}],
+            "freq" : 10.0}],
     )
 
     # Covariance filter node to publish IMU + LiDAR + wheel encoder covariance
--- src/x1_bringup/x1_bringup/covariance_filter.py
+++ src/x1_bringup/x1_bringup/covariance_filter.py
@@ -6,11 +6,16 @@ from nav_msgs.msg import Odometry
 
 import copy
 import signal
+import numpy as np
 
 
 class CovarianceFilter(Node):
     def __init__(self):
         super().__init__("covariance_filter")
+
+        # ===== Parameters =====
+        self.yaw_filter_alpha = 0.9
+        self.yaw_filtered = None
 
         # ===== ROS Interface =====
         self.imu_sub        = self.create_subscription(Imu, "imu/data_raw", self.imu_callback, 10)
@@ -25,24 +30,24 @@ class CovarianceFilter(Node):
         # orientation covariance row majors, in the order of (roll, pitch, yaw)
         self.imu_orientation_covariance = [1.0, 0.0, 0.0,
                                            0.0, 1.0, 0.0,
-                                           0.0, 0.0, 1e-2]
+                                           0.0, 0.0, 1e-1]
         # angular velocity covariance row majors, in the order of (vroll, vpitch, vyaw)
         self.imu_angular_velocity_covariance = [1.0, 0.0, 0.0,
                                                 0.0, 1.0, 0.0,
-                                                0.0, 0.0, 1e-2]
+                                                0.0, 0.0, 5e-2]
         # linear acceleratioin covariance row majors, in the order of (ax, ay, az)
-        self.imu_linear_acceleration_covariance = [1e-2, 0.0, 0.0,
-                                                   0.0, 1e-2, 0.0,
-                                                   0.0, 0.0, 1e-2]
+        self.imu_linear_acceleration_covariance = [5e-2, 0.0,  0.0,
+                                                   0.0,  5e-2, 0.0,
+                                                   0.0,  0.0,  1e-2]
         
         # LiDAR covariances - as per https://docs.ros2.org/foxy/api/nav_msgs/msg/Odometry.html
         # pose covariance, in the order of (x, y, z, r, p, y)
-        self.lidar_pose_covariance = [0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                      0.0, 0.1, 0.0, 0.0, 0.0, 0.0,
-                                      0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
-                                      0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-                                      0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-                                      0.0, 0.0, 0.0, 0.0, 0.0, 1e-2]
+        self.lidar_pose_covariance = [1e-2, 0.0,  0.0, 0.0, 0.0, 0.0,
+                                      0.0,  1e-2, 0.0, 0.0, 0.0, 0.0,
+                                      0.0,  0.0,  1e9, 0.0, 0.0, 0.0,
+                                      0.0,  0.0,  0.0, 1e9, 0.0, 0.0,
+                                      0.0,  0.0,  0.0, 0.0, 1e9, 0.0,
+                                      0.0,  0.0,  0.0, 0.0, 0.0, 2e-1]
         # twist covariance, in the order of (vx, vy, vz, vr, vp, vy)
         self.lidar_twist_covariance = [1e-2, 0.0,  0.0,  0.0,  0.0,  0.0,
                                        0.0,  1e-2, 0.0,  0.0,  0.0,  0.0,
@@ -54,12 +59,12 @@ class CovarianceFilter(Node):
         # Wheel velocity covariances
         # as per https://docs.ros.org/en/humble/p/geometry_msgs/msg/TwistWithCovarianceStamped.html
         # twist covariance, in the order of (vx, vy, vz, vr, vp, vy)
-        self.wheel_twist_covariance = [0.1,  0.0,  0.0,  0.0,  0.0,  0.0,
-                                       0.0,  0.1,  0.0,  0.0,  0.0,  0.0,
-                                       0.0,  0.0,  0.1,  0.0,  0.0,  0.0,
-                                       0.0,  0.0,  0.0,  1.0,  0.0,  0.0,
-                                       0.0,  0.0,  0.0,  0.0,  1.0,  0.0,
-                                       0.0,  0.0,  0.0,  0.0,  0.0,  1.0]
+        self.wheel_twist_covariance = [1e-1, 0.0,  0.0,  0.0,  0.0,  0.0,
+                                       0.0,  1e-1, 0.0,  0.0,  0.0,  0.0,
+                                       0.0,  0.0,  1e9,  0.0,  0.0,  0.0,
+                                       0.0,  0.0,  0.0,  1e9,  0.0,  0.0,
+                                       0.0,  0.0,  0.0,  0.0,  1e9,  0.0,
+                                       0.0,  0.0,  0.0,  0.0,  0.0,  4e-1]
 
         self.get_logger().info('Covariance filter node is running')
         
@@ -73,7 +78,22 @@ class CovarianceFilter(Node):
         self.imu_pub.publish(msg_covariance)
 
     def lidar_callback(self, msg: Odometry):
+        # extract yaw from quaternion
+        q = msg.pose.pose.orientation
+        siny = 2.0 * (q.w * q.z + q.x * q.y)
+        cosy = 1.0 - 2.0 * (q.y**2 + q.z**2)
+        yaw_raw = np.arctan2(siny, cosy)
+
+        if self.yaw_filtered is None:
+            self.yaw_filtered = yaw_raw
+        else:
+            self.yaw_filtered = (self.yaw_filter_alpha) * self.yaw_filtered + (1.0 - self.yaw_filter_alpha) * yaw_raw 
+
         msg_covariance = copy.deepcopy(msg)
+        msg_covariance.pose.pose.orientation.x = 0.0
+        msg_covariance.pose.pose.orientation.y = 0.0
+        msg_covariance.pose.pose.orientation.z = np.sin(self.yaw_filtered / 2)
+        msg_covariance.pose.pose.orientation.w = np.cos(self.yaw_filtered / 2)
         msg_covariance.pose.covariance = self.lidar_pose_covariance
         msg_covariance.twist.covariance = self.lidar_twist_covariance
         self.lidar_pub.publish(msg_covariance)
